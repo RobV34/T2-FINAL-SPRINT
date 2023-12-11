9ae67d9ea4afc87dee04bdef5d838103baf2c1e4
Rob Vatcher <rob.vatcher@keyin.com>
2023-12-11 18:58:25 -0330

    commit all

---
+++ clocks/src/components/contact/Contact.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about Custom Clocks and our mission.</p>
+      {/* Additional content */}
+    </div>
+  );
+};
+
+export default About;
+
