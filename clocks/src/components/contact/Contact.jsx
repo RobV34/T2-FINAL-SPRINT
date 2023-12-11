6d2f4a6c0a9bceb37564bb91589cd5028658f88b
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2023-12-11 18:52:33 -0330

    Update Contact.jsx

---
+++ clocks/src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Welcome to Custom Clocks</h1>
+      <p>Find the perfect clock for your home.</p>
+      {/* Additional content */}
+    </div>
+  );
+};
+
+export default Contact;
