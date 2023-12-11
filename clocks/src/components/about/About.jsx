0490473efa023ef0ceb16468cf67a6126552b5b7
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2023-12-11 14:39:02 -0330

    Add files via upload

---
+++ clocks/src/components/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about Custom Clocks and our mission.</p>
+      {/* Additional content */}
+    </div>
+  );
+};
+
+export default About;
