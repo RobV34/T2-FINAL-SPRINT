0ab6d19446b82295d99fa05514ba329f0b889f6a
Rob Vatcher <135687070+RobV34@users.noreply.github.com>
2023-12-12 14:35:10 -0330

    Add files via upload

---
+++ clocks/src/components/clock/Clock.jsx
@@ -0,0 +1,33 @@
+
+
+
+import React, { useEffect, useState } from 'react';
+import './Clock.css'; // CSS file for clock styles
+
+const Clock = () => {
+    const [time, setTime] = useState(new Date()); // initialize time with current time
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date()); // update time
+        }, 1000);
+
+        return () => {
+            clearInterval(timer); // cleanup function
+        };
+    }, []); 
+
+    const secondAngle = time.getSeconds() * 6; // 360 / 60 = 6
+    const minuteAngle = time.getMinutes() * 6 + secondAngle / 60; // 360 / 60 = 6
+    const hourAngle = (time.getHours() % 12) * 30 + minuteAngle / 12; // 360 / 12 = 30
+
+    return (
+        <div id="clock">
+            <div id="hour-hand" className="hand" style={{ transform: `rotate(${hourAngle}deg)` }}></div> 
+            <div id="minute-hand" className="hand" style={{ transform: `rotate(${minuteAngle}deg)` }}></div>
+            <div id="second-hand" className="hand" style={{ transform: `rotate(${secondAngle}deg)` }}></div>
+        </div>
+    );
+};
+
+export default Clock;
